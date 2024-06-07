57976f47356170d8c9f8033a2594431e980e3e0f
AsmaaShabana22 <dr.asmaa.moh.shabana@gmail.com>
2024-06-07 21:44:02 +0300

    add a new line to the main.java file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            
         }
     }
 }
